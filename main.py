335b61f1eb939bc8f1c016c8345f4395c1790af3
divyesh2006d <divyesh2006d@gmail.com>
2025-09-17 21:36:39 +0530

    add dialog boxes and pop-ups

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from tkinter import *
-
+from tkinter import messagebox
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 def save():
+
+    # messagebox.showinfo('Info', 'Password has been saved.')
+
+    if len(inp_web.get()) != 0 and len(inp_passw.get()) != 0:
+        messagebox.askokcancel(title= f'{inp_web.get()}', message=f'These are the details entered :\n Email : {inp_email.get()}\n'
+                                                  f'Password : {inp_passw.get()} \n it is ok to save ?')
+    else:
+        messagebox.showerror('Error', 'Something went wrong.')
+
     with open('password.txt', 'a') as file:
         file.write(f'{inp_web.get()},{inp_email.get()},{inp_passw.get()}\n')
     inp_web.delete(0, END)
-    inp_email.delete(0, END)
     inp_passw.delete(0, END)
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -32,11 +40,11 @@ passw = Label(window, text='Password :')
 passw.grid(row=3, column=0)
 
 # butten
-G_btn = Button(text='Generate Password', command=window.quit)
+G_btn = Button(text='Generate Password', command=generate_password)
 G_btn.grid(row=3, column=2)
 
 add_btn = Button(text='Add Password', command=save,width=36)
-add_btn.grid(row=4, column=1)
+add_btn.grid(row=4, column=1,columnspan=2)
 
 # inputs
 inp_web = Entry(width=35)
@@ -47,7 +55,7 @@ inp_email = Entry(width=35)
 inp_email.grid(row=2, column=1,columnspan=2)
 inp_email.insert(0,'divyesh@gmail.com')
 
-inp_passw = Entry(width=21, show='*')
-inp_passw.grid(row=3, column=1,columnspan=2)
+inp_passw = Entry(width=21)
+inp_passw.grid(row=3, column=1)
 
 window.mainloop()
